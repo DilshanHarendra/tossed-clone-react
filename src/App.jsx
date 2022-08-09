3236bfc5e3bfe1d55f08e5180cebc22571ad1692
DilshanHarendra <harendracripto@gmail.com>
2022-08-09 23:36:37 +0530

    create sample project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './assets/tailwind.css'
+import MainLayout from "./layouts/MainLayout";
+
+function App() {
+
+  return (
+    <MainLayout/>
+  )
+}
+
+export default App
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Routers from "../routers/Routers";
+import {  Link } from "react-router-dom";
+function MainLayout(){
+
+
+    return(<>
+       <div className="container text-lg mx-auto">
+           <nav>
+               <ul>
+
+                   <li><Link to="/">Home</Link></li>
+                   <li><Link to="/about">About</Link></li>
+
+
+               </ul>
+           </nav>
+           <Routers/>
+       </div>
+
+
+    </>)
+}export default MainLayout
