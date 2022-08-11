eeaa019bfbe1e60d22f58e5b10aedb7af90c954e
DilshanHarendra <52866622+DilshanHarendra@users.noreply.github.com>
2022-08-11 17:29:06 +0000

    Deploying to deploy from @ DilshanHarendra/tossed-clone-react@1fde1c60257f57c2bb9f3adb45565e6d571f3d58 🚀

---
+++ static/AboutPage.ebd4482a.js
@@ -0,0 +1,1 @@
+import{j as i,a as e}from"./index.62690ab6.js";function s(){return i("div",{children:[e("h2",{children:"About"}),e("p",{children:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, aliquid amet at esse excepturi expedita fugit inventore iure maxime necessitatibus nisi possimus praesentium qui quidem quo tenetur velit vero voluptate."})]})}export{s as default};
