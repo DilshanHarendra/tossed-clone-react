a1195240754878f6e71a53445ff6e4f0861d2687
DilshanHarendra <harendracripto@gmail.com>
2022-08-11 23:02:44 +0530

    remove console logs

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import {BrowserRouter} from 'react-router-dom';
 // eslint-disable-next-line
 import 'swiper/css/bundle';
 
+if (import.meta.env.VITE_ENV=='prod') {
+    console.log = console.warn = console.error = () => {};
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter basename={import.meta.env.VITE_ENV=='dev'?'':'/tossed-clone-react'}>
